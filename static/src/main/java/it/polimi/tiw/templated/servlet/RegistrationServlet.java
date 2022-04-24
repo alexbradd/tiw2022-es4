a0b3a3327b21b424ee3587b4ebc64ac80d196371
Alexandru Gabriel Bradatan <alexandrugabriel.bradatan@mail.polimi.it>
2022-04-25 00:01:22 +0200

    feat(static): add registration page

---
+++ static/src/main/java/it/polimi/tiw/templated/servlet/RegistrationServlet.java
@@ -0,0 +1,40 @@
+package it.polimi.tiw.templated.servlet;
+
+
+import it.polimi.tiw.api.UserApi;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Endpoint for user registration. It accepts POST requests and forwards them to
+ * {@link UserApi#register(HttpServletRequest)}. In case of success, the servlet will redirect to the templated login
+ * page. Otherwise, it will redirect to the register page with an error flag in the query string: "e". The various
+ * values for "e" are:
+ *
+ * <ol>
+ *     <li>"user": if the form contained errors (parameters missing or malformed)</li>
+ *     <li>"conflict": if there is already an user with the same username</li>
+ *     <li>"server": if the server was unable to fulfill the request (e.g. database is unavailable)</li>
+ * </ol>
+ */
+@WebServlet("/registerUser")
+public class RegistrationServlet extends HttpServlet {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String redirect = UserApi.register(req)
+                .match((u) -> "/login",
+                        (e) -> "/register.html?e=" + switch (e.statusCode()) {
+                            case 400 -> "user";
+                            case 409 -> "conflict";
+                            default -> "server";
+                        });
+        resp.sendRedirect(redirect);
+    }
+}
