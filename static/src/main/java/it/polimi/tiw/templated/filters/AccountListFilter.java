30fe47eaa7f1aae206dc73985d0b8a5b1b54b2fc
Alexandru Gabriel Bradatan <alexandrugabriel.bradatan@mail.polimi.it>
2022-05-17 20:01:32 +0200

    refactor(static): use functional-like api better in AccountListFilter

--- static/src/main/java/it/polimi/tiw/templated/filters/AccountListFilter.java
+++ static/src/main/java/it/polimi/tiw/templated/filters/AccountListFilter.java
@@ -3,7 +3,6 @@ package it.polimi.tiw.templated.filters;
 import it.polimi.tiw.api.AccountFacade;
 import it.polimi.tiw.api.beans.Account;
 import it.polimi.tiw.api.beans.User;
-import it.polimi.tiw.api.functional.ApiResult;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -12,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.List;
 
 /**
  * Adds as the 'accountList' attributes the list of all the {@link Account} associated with the {@link User} stored
@@ -26,13 +24,11 @@ public class AccountListFilter extends HttpFilter {
     public void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpSession session = req.getSession(false);
         User user = (User) session.getAttribute("user");
-        ApiResult<List<Account>> result = AccountFacade.withDefaultObjects().ofUser(user);
-        if (result.match(__ -> true, __ -> false)) {
-            List<Account> l = result.get();
-            req.setAttribute("accountList", l);
-        } else {
-            req.setAttribute("accountList", null);
-        }
+        AccountFacade.withDefaultObjects()
+                .ofUser(user)
+                .consume(
+                        l -> req.setAttribute("accountList", l),
+                        e -> req.setAttribute("accountList", null));
         chain.doFilter(req, res);
     }
 }
