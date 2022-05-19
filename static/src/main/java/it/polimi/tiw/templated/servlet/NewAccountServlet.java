eb29aabf40fda4eefe65de3b04a210cd9115f3a7
Alexandru Gabriel Bradatan <alexandrugabriel.bradatan@mail.polimi.it>
2022-05-19 15:52:53 +0200

    feat(static): lockdown /createAccount to current user only
    Before you could create a new account for any arbitrary account. Now you
    can create a new account only for the user registered in the session.
    If no session or if no user are available, a 403 error is sent.

--- static/src/main/java/it/polimi/tiw/templated/servlet/NewAccountServlet.java
+++ static/src/main/java/it/polimi/tiw/templated/servlet/NewAccountServlet.java
@@ -9,13 +9,15 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
- * Endpoint for creating a new {@link Account} for the {@link User} with the ID specified in the request parameters. It
- * requires a User id encoded in url-safe base64 as a parameter. After creating the new account, it will redirect to
- * {@code index.html}. If an error occurred, it will be signaled to the redirect using the query string parameter
- * {@code e}.
+ * Endpoint for creating a new {@link Account} for the currently logged in {@link User}. It requires a Session with a
+ * valid user. If such requirements are not met, the request will be rejected with a 403.
+ * <p>
+ * After creating the new account, it will redirect to {@code index.html}. If an error occurred, it will be signaled to
+ * the redirect using the query string parameter.
  */
 @WebServlet("/createAccount")
 public class NewAccountServlet extends HttpServlet {
@@ -24,22 +26,27 @@ public class NewAccountServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String id = req.getParameter("userId");
-        String red;
-
-        if (id == null)
-            red = "/index.html?e=user";
-        else {
-            red = ProductionConnectionRetriever.getInstance()
-                    .with(c -> AccountFacade.withDefaultObjects(c).createFor(id))
-                    .match(
-                            a -> "/index.html",
-                            e -> "/index.html?e=" + switch (e.statusCode()) {
-                                case 400, 404 -> "user";
-                                default -> "server";
-                            }
-                    );
+        HttpSession session = req.getSession(false);
+        if (session == null) {
+            resp.sendError(403);
+            return;
         }
+
+        User u = (User) session.getAttribute("user");
+        if (u == null) {
+            resp.sendError(403);
+            return;
+        }
+
+        String red = ProductionConnectionRetriever.getInstance()
+                .with(c -> AccountFacade.withDefaultObjects(c).createFor(u.getBase64Id()))
+                .match(
+                        a -> "/index.html",
+                        e -> "/index.html?e=" + switch (e.statusCode()) {
+                            case 400, 404 -> "user";
+                            default -> "server";
+                        }
+                );
         resp.sendRedirect(red);
     }
 }
