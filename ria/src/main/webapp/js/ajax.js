212dad2ca6c98fa63bfb18539bbd0f9f0b9f3842
Alexandru Gabriel Bradatan <alexandrugabriel.bradatan@mail.polimi.it>
2022-06-04 00:29:19 +0200

    feat(ria): implement Ajax.authenticatedPost
    `Ajax.authenticatedPost(url, objectData, callback)` is convenience
    method that automatically handles adding the authentication token
    to the request data and the refresh-and-retry flow in case of a 401
    response.

--- ria/src/main/webapp/js/ajax.js
+++ ria/src/main/webapp/js/ajax.js
@@ -19,6 +19,55 @@ function Ajax() {
     this.post = function (url, data, callback, setJson = true) {
         this._makeReq("POST", url, data, setJson, callback);
     }
+
+    this._refreshTokenAndRetry = function (method, url, objectData, callback) {
+        this._makeReq("GET", "/api/auth/refresh", null, false, (req1) => {
+            if (req1.readyState !== XMLHttpRequest.DONE)
+                return;
+            if (req1.status !== 200)
+                callback(req1, true);
+            let res = JSON.parse(req1.responseText);
+            window.sessionStorage.setItem("token", res.token);
+            this._makeReq(
+                method,
+                url,
+                JSON.stringify({
+                    ...objectData,
+                    token: res.token
+                }),
+                true,
+                (req2) => callback(req2, false));
+        })
+    }
+
+    this.authenticatedPost = function (url, objectData, callback) {
+        if (!isLoggedIn())
+            throw new Error("User is not logged in");
+        this._makeReq(
+            "POST",
+            url,
+            JSON.stringify({
+                ...objectData,
+                token: window.sessionStorage.getItem("token")
+            }),
+            true,
+            (req) => {
+                if (req.readyState === XMLHttpRequest.DONE) {
+                    switch (req.status) {
+                        case 200:
+                            callback(req, false);
+                            break;
+                        case 401:
+                            this._refreshTokenAndRetry("POST", url, objectData, callback);
+                            break;
+                        default:
+                            callback(req, false);
+                    }
+                } else
+                    callback(req, false);
+            }
+        );
+    }
 }
 
 function convertFormDataToJSON(formData) {
